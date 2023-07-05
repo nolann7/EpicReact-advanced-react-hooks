3f32357ab8984676768fbc4e53a08b9974c0e96f
nolann7 <nolann709@gmail.com>
2023-07-05 15:49:07 +0300

    1 exercise - 2-3 extras

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -14,14 +14,23 @@ function Counter({initialCount = 0, step = 1}) {
   //   }
   // }
 
-  const [count, setCount] = useReducer(
-    (prevState, newState) => prevState + newState,
-    initialCount,
-  )
+  // const [count, setCount] = useReducer(
+  //   (prevState, newState) => prevState + newState,
+  //   initialCount,
+  // )
   // const [count, dispatchCount] = useReducer(reducer, initialCount)
+  function countReducer(prevState, newState) {
+    return typeof newState === 'function' ? newState(prevState) : newState
+  }
 
-  const increment = () => setCount(step)
+  const [state, setState] = useReducer(countReducer, {count: initialCount})
+  const {count} = state
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
+
+  // const increment = () => setCount(step)
+  // return <button onClick={increment}>{count}</button>
   // return (
   //   <button onClick={() => dispatchCount({type: 'increment'})}>{count}</button>
   // )
