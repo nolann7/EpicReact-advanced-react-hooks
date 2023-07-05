568744c85ef54b69f85d3ae7e1faa3b547dd4c7c
nolann7 <nolann709@gmail.com>
2023-07-05 15:54:38 +0300

    1 exercise - 4 extra

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,36 +4,41 @@
 import {useReducer} from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
-  // function reducer(state, action) {
-  //   switch (action.type) {
-  //     case 'increment': {
-  //       return state + step
-  //     }
-  //     default:
-  //       throw Error('Unknown action: ' + action.type)
-  //   }
-  // }
-
   // const [count, setCount] = useReducer(
   //   (prevState, newState) => prevState + newState,
   //   initialCount,
   // )
   // const [count, dispatchCount] = useReducer(reducer, initialCount)
-  function countReducer(prevState, newState) {
-    return typeof newState === 'function' ? newState(prevState) : newState
-  }
+  // 2-3 extra
+  // function countReducer(prevState, newState) {
+  //   return typeof newState === 'function' ? newState(prevState) : newState
+  // }
 
-  const [state, setState] = useReducer(countReducer, {count: initialCount})
-  const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
-  return <button onClick={increment}>{count}</button>
+  // const [state, setState] = useReducer(countReducer, {count: initialCount})
+  // const {count} = state
+  // const increment = () =>
+  //   setState(currentState => ({count: currentState.count + step}))
+  // return <button onClick={increment}>{count}</button>
 
   // const increment = () => setCount(step)
   // return <button onClick={increment}>{count}</button>
   // return (
   //   <button onClick={() => dispatchCount({type: 'increment'})}>{count}</button>
   // )
+  // 4 extra
+  function countReducer(state, action) {
+    switch (action.type) {
+      case 'INCREMENT': {
+        return {count: state.count + action.step}
+      }
+      default:
+        throw Error('Unknown action: ' + action.type)
+    }
+  }
+  const [state, dispatch] = useReducer(countReducer, {count: initialCount})
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', step})
+  return <button onClick={increment}>{count}</button>
 }
 
 function App() {
