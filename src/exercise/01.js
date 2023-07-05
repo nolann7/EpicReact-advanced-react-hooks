829642894fb531466e8765cf6a9d28b7585c1750
nolann7 <nolann709@gmail.com>
2023-07-05 15:43:03 +0300

    1 exercise - 1 extra

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,31 +4,27 @@
 import {useReducer} from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
-  function reducer(state, action) {
-    switch (action.type) {
-      case 'increment': {
-        return state + step
-      }
-      default:
-        throw Error('Unknown action: ' + action.type)
-    }
-    // if (action.type === 'increment') {
-    //   let newState = state + step
-    //   return newState
-    // }
-  }
+  // function reducer(state, action) {
+  //   switch (action.type) {
+  //     case 'increment': {
+  //       return state + step
+  //     }
+  //     default:
+  //       throw Error('Unknown action: ' + action.type)
+  //   }
+  // }
 
-  // const [count, setCount] = useReducer(
-  //   (prevState, newState) => newState,
-  //   initialCount,
-  // )
-  const [count, dispatchCount] = useReducer(reducer, initialCount)
-
-  // const increment = () => setCount(count + step)
-  // return <button onClick={increment}>{count}</button>
-  return (
-    <button onClick={() => dispatchCount({type: 'increment'})}>{count}</button>
+  const [count, setCount] = useReducer(
+    (prevState, newState) => prevState + newState,
+    initialCount,
   )
+  // const [count, dispatchCount] = useReducer(reducer, initialCount)
+
+  const increment = () => setCount(step)
+  return <button onClick={increment}>{count}</button>
+  // return (
+  //   <button onClick={() => dispatchCount({type: 'increment'})}>{count}</button>
+  // )
 }
 
 function App() {
